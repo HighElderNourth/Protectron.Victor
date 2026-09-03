32067cd8f6f904482307009decc3168d46123c2f
HighElderNourth <ja922057@gmail.com>
2026-09-03 15:29:00 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Searching for Toasters.",     // this is what people actually see
+        state: "Searching for Toasters, Fusion Cells, and Chems",     // this is what people actually see
         type: 2,               // Custom
       },
     ],
